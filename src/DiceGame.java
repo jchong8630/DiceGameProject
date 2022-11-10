3e0405f46e983c7d41dd0bc205bc9de89609ab22
BT_4N2_18 <jxu5406@bths.edu>
2022-11-10 12:07:11 -0500

    Finished game code

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
+
 public class DiceGame {
-    private int money;
+    private double money;
     private int turns;
     private int diceRoll;
     private String result;
-    private int prev;
+    private int prev = rollDice();
 
     public DiceGame(){
         money = 50;
@@ -19,7 +21,7 @@ public class DiceGame {
         turns++;
     }
 
-    public int getMoney(){
+    public double getMoney(){
         return money;
     }
     public int getTurns(){
@@ -28,23 +30,43 @@ public class DiceGame {
     public int getPrev(){
         return prev;
     }
-    public void decreaseMoney(int bet){
+    public void decreaseMoney(double bet){
         money -= bet;
+    }
+    public boolean choiceChecker(String choice){
+        choice = choice.toLowerCase();
+        if ((choice.contains("higher")) || (choice.contains("lower"))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean betChecker(double bet){
+        if (bet != Math.round(bet * 100)/100.00){
+            return false;
+        }
+        if (bet <= money){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 
-    public void increaseMoney(int bet){
+    public void increaseMoney(double bet){
         money += bet;
     }
 
-    public void game(String choice, int bet){
+    public void game(String choice, double bet){
         String correct = "";
         diceRoll = rollDice();
         if (prev > diceRoll){
             correct = "lower";
         }
         else if (prev < diceRoll){
-            correct = "Higher";
+            correct = "higher";
         }
         else if (prev == diceRoll){
             correct = choice;
@@ -62,7 +84,7 @@ public class DiceGame {
     }
 
     public String toString() {
-        String gameMessage = "Dice Roll: " + diceRoll + "\n" + result + "\nMoney: " + money + "   Turns: " + turns;
+        String gameMessage = "Dice Roll: " + diceRoll + "\n" + result + "\nMoney: " + String.format("%.02f", money) + "   Turns: " + turns + "\n";
         return gameMessage;
     }
 }
--- src/DiceGameRunner.java
+++ src/DiceGameRunner.java
@@ -4,16 +4,36 @@ public class DiceGameRunner {
         Scanner s = new Scanner(System.in);
         DiceGame game = new DiceGame();
         String choice;
-        int bet;
-        int prev = game.rollDice();
+        double bet;
+        System.out.println("Welcome To Dice Higher Or Lower Game!");
         while ((game.getMoney() > 0) && (game.getMoney() < 500)){
-            System.out.println(prev);
+            System.out.println("Current Roll: " + game.getPrev() + "   Money: " + String.format("%.02f",game.getMoney()));
             System.out.print("Higher Or Lower: ");
             choice = s.next();
+            while (game.choiceChecker(choice) == false){
+                System.out.print("Please Enter \"Higher\" Or \"Lower\": ");
+                choice = s.next();
+            }
             System.out.print("Bet: ");
-            bet = s.nextInt();
+            bet = s.nextDouble();
+            while (game.betChecker(bet) == false){
+                if (bet != Math.round(bet * 100)/100.00){
+                    System.out.println("Please Input A Valid Bet!");
+                }
+                else{
+                    System.out.println("Bet Exceeds Current Balance!");
+                }
+                System.out.print("Bet: ");
+                bet = s.nextDouble();
+            }
             game.game(choice, bet);
             System.out.println(game.toString());
+        }
+        if (game.getMoney() >= 500){
+            System.out.println("Congratulations! You Beat The Game In " + game.getTurns() + " Turns!");
+        }
+        else{
+            System.out.println("You Lost All Your Money!");
         }
     }
 }
